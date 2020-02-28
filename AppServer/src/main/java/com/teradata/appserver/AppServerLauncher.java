35eda817ba6ea7b69e28bfa286e4a474b947f319
Meeraj Kanaparthi <meeraj.kanaparthi@teradata.com>
2020-02-28 11:37:25 +0530

    first commit

---
+++ AppServer/src/main/java/com/teradata/appserver/AppServerLauncher.java
@@ -0,0 +1,12 @@
+package com.teradata.appserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppServerLauncher {
+    public static void main(String[] args) {
+        SpringApplication.run(AppServerLauncher.class, args);
+    }
+
+}
---
+++ src/main/java/com/teradata/fightcrime/FightcrimeApplication.java
@@ -0,0 +1,13 @@
+package com.teradata.fightcrime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FightcrimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FightcrimeApplication.class, args);
+    }
+
+}
